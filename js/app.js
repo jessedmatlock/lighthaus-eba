7b22be58529f3cbdb99cc65dd2b5aa76f1ce9074
Jesse Matlock <jesse@revivemarketing.org>
2015-05-16 10:57:59 -0500

    added profile page, update CSS and footer

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	$(document).foundation();
 	// smooth scrolling for any link targetting an existing ID
 	$(function() {
-	  $('a[href*=#]:not([href=#])').click(function() {
+	  $('a[href*=#]:not([href=#]):not([href*="#tab"])').click(function() {
 	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
 	      var target = $(this.hash);
 	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
